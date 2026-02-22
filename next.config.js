70d678e7ad007ebda75d6c770ed0880a8862f614
Václav Slepr <mrockofficialytb@gmail.com>
2026-02-22 01:11:28 +0100

    add clean next config utf8

---
+++ next.config.js
@@ -0,0 +1,7 @@
+﻿/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true }
+};
+
+module.exports = nextConfig;
