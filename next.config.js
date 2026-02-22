8b6674608d46f68f11c16baceff46609401d5103
Václav Slepr <mrockofficialytb@gmail.com>
2026-02-22 01:15:34 +0100

    config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
-/** @type {import("next").NextConfig} */
+﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*'],
+    },
+  },
 };
 
 module.exports = nextConfig;
